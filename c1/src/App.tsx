810476e12ca61dc3e98ef0395a20dce2ee08fc1a
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-05-23 17:08:48 +0530

    data

--- c1/src/App.tsx
+++ c1/src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Nav } from "./part1/components/Nav";
+import { CardDetails } from "./part1/components/CardDetails";
+import { Checkout } from "./part1/components/Checkout";
+// import { Body } from "./part1/components/Checkout";
+import { Routes,Route  } from  "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-
+      <Nav />
+      <Routes>
+        <Route path="/" element={<CardDetails />}></Route>
+        <Route path="/:id" element={<CardDetails />}>
+          {" "}
+        </Route>
+        <Route path="checkout" element={<Checkout />}></Route>
+      </Routes>
     </div>
   );
 }
--- c1/src/index.tsx
+++ c1/src/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
 
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom'
+import App from './App';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </BrowserRouter>
+ 
+    <BrowserRouter>
+    <App/>
+    </BrowserRouter>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
